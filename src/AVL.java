2402ff9c90086c23ed8daa60d9349f9d25854943
Yanay <yanayshem@gmail.com>
2020-06-08 03:03:30 +0300

    AVL update with recInsert

--- src/AVL.java
+++ src/AVL.java
@@ -1,8 +1,106 @@
+import java.io.*;
+import java.util.*;
+
 public class AVL<T> {
-	
-	public void insert(int key, T data){
-		// TODO
+
+	private int height (AVLNode<T> N) {
+		if (N == null)
+			return 0;
+		return N.getHeight();
 	}
+
+
+
+
+	public void insert(int key, T data) {
+
+		AVLNode<T> node = new AVLNode<T>(key, data);
+		insertRec(node, key);
+	}
+
+	private AVLNode<T> insertRec(AVLNode<T> node, int key){
+		/* 1.  Perform the normal BST rotation */
+
+
+		if (key < node.key)
+			node.left  = insertRec(node.left, key);
+		else
+			node.right = insertRec(node.right, key);
+
+		/* 2. Update height of this ancestor node */
+		node.height = Math.max(height(node.left), height(node.right)) + 1;
+
+	/* 3. Get the balance factor of this ancestor node to check whether
+	   this node became unbalanced */
+		int balance = getBalance(node);
+
+		// If this node becomes unbalanced, then there are 4 cases
+
+		// Left Left Case
+		if (balance > 1 && key < node.left.key)
+			return rightRotate(node);
+
+		// Right Right Case
+		if (balance < -1 && key > node.right.key)
+			return leftRotate(node);
+
+		// Left Right Case
+		if (balance > 1 && key > node.left.key)
+		{
+			node.left =  leftRotate(node.left);
+			return rightRotate(node);
+		}
+
+		// Right Left Case
+		if (balance < -1 && key < node.right.key)
+		{
+			node.right = rightRotate(node.right);
+			return leftRotate(node);
+		}
+
+		/* return the (unchanged) node pointer */
+		return node;
+	}
+
+	private AVLNode<T> rightRotate(AVLNode<T> y) {
+		AVLNode<T> x = y.left;
+		AVLNode<T> T2 = x.right;
+
+		// Perform rotation
+		x.right = y;
+		y.left = T2;
+
+		// Update heights
+		y.height = Math.max(height(y.left), height(y.right))+1;
+		x.height = Math.max(height(x.left), height(x.right))+1;
+
+		// Return new root
+		return x;
+	}
+
+	private AVLNode<T> leftRotate(AVLNode<T> x) {
+		AVLNode<T> y = x.right;
+		AVLNode<T> T2 = y.left;
+
+		// Perform rotation
+		y.left = x;
+		x.right = T2;
+
+		//  Update heights
+		x.height = Math.max(height(x.left), height(x.right))+1;
+		y.height = Math.max(height(y.left), height(y.right))+1;
+
+		// Return new root
+		return y;
+	}
+
+	// Get Balance factor of node N
+	private int getBalance(AVLNode<T> N) {
+		if (N == null)
+			return 0;
+		return height(N.left) - height(N.right);
+	}
+
 
 	public T search(int key){
 		//TODO
--- src/AVLNode.java
+++ src/AVLNode.java
@@ -1,32 +1,55 @@
 
 public class AVLNode<T> {
+	protected   AVLNode<T> left, right, father;
+	protected int height = 1;
+	protected int key;
+	protected T data;
 
-	
+	public AVLNode (int key, T data) {
+		this.key = key;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
 	public AVLNode<T> getLeftChild(){
-		//TODO
-		return null;
+		return left;
 	}
 	
 	public AVLNode<T> getRightChild(){
-		//TODO
-		return null;
+		return right;
 	}
 	
 	public AVLNode<T> getFather(){
-		//TODO
-		return null;
+		return father;
 	}
 	
 	public int getKey(){
-		//TODO
-		return 0;
+		return this.key;
 	}
 	
 	public T getData(){
-		//TODO
-		return null;
+		return this.data;
 	}
-	
+
+	public void setLeftChild(AVLNode<T> left) {
+		this.left = left;
+	}
+
+	public void setRightChild(AVLNode<T> right) {
+		this.right = right;
+	}
+
+	public void setFather(AVLNode<T> father) {
+		this.father = father;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+
 	@Override
 	public String toString() {
 		
