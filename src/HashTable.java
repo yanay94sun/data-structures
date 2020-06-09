2bb672f0234db42a234e3bd993bf25c93caeb629
refael-farjun <refaelfarjun5@gmail.com>
2020-06-09 07:37:05 +0300

    Finish HashTable!

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,15 +1,53 @@
 import java.util.LinkedList;
-
-
 public class HashTable {
-	
+	private int size = 11; // NEED TO CHECK WHICH NUMBER REPLACE M???!!!!!!
+	private LinkedList[] table;
+
+	public HashTable() {
+		table = new LinkedList[size];
+		for (int i = 0; i < size; i++) {
+			table[i] = null;
+		}
+	}
 	public void insert(ObjectWithCoordinates object){
-		//TODO
+		int hash = h(object);
+		if (table[hash] == null){
+			table[hash] =  new LinkedList();
+			table[hash].addFirst(object);
+		}
+		else {
+			LinkedList input = table[hash];
+			input.addFirst(object);
+
+		}
 	}
 
 	public ObjectWithCoordinates search(int x, int y){
-		//TODO
-		return null;
+		int hash = (x + y)%11;
+		if (table[hash] == null){
+			return null;
+		}
+		else {
+
+			LinkedList input = table[hash];
+			int i = 0;
+			while ((((ObjectWithCoordinates) input.get(i)).getX() != x) && ((((ObjectWithCoordinates) input.get(i)).getY()) != y)){
+				if (input.getLast() == input.get(i)){
+					return null;
+				}
+				i++;
+				}
+			return (ObjectWithCoordinates)input.get(i);
+				}
+	}
+
+
+
+
+
+
+	public int h(ObjectWithCoordinates point){
+		return (point.getX() + point.getY())%11;
 	}
 }
 
